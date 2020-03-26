979b3f773b2bd26887b375da498762b2dc0949d9
ihaveint <ihaveint@gmail.com>
2020-03-26 04:47:15 +0430

    Resolve bug in user_info request

--- codeforces_publicAPI_provider.py
+++ codeforces_publicAPI_provider.py
@@ -1,7 +1,13 @@
 from urllib.request import Request, urlopen
+import urllib
 
 def user_info(user: str):
-	url = 'https://codeforces.com/api/user.info?handles={}'.format(user)
-	req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	response =  urlopen(req)
-	return response
+    url = 'https://codeforces.com/api/user.info?handles={}'.format(user)
+    print("url is : ")
+    print(url)
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0', 'Content-Type':'application/json'})
+    try :
+        response = urlopen(req)
+        return response.read().decode("utf-8")
+    except urllib.error.HTTPError as p:
+        return ""
--- helpers.py
+++ helpers.py
@@ -3,9 +3,9 @@ from codeforces_publicAPI_provider import user_info
 import json
 
 def user_exists(user: str) -> bool :
-	httpresponse = user_info(user)
-	print(httpresponse.status)
-	if httpresponse.status != 200:
+	http_response_body = user_info(user)
+	print(http_response_body)
+	if http_response_body == "":
 		return False
 	return True
 
